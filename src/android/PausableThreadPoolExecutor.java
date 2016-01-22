a183b028873040f4cbe60f8cfb33a5d259ee4af6
Richard Lewin <richard.lewin@commontime.com>
2016-01-22 12:45:36 +0000

    Version update and bug fixes

--- src/android/PausableThreadPoolExecutor.java
+++ src/android/PausableThreadPoolExecutor.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package com.commontime.cordova.beacon;
+package com.commontime.plugin;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
